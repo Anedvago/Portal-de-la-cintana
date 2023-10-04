d0364b721c65baefe469071fd1300a8d8d3f00e0
Anedvago <61481830+Anedvago@users.noreply.github.com>
2023-10-03 21:31:41 -0500

    first set y login

---
+++ src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    loadChildren: () =>
+      import('./modules/auth/auth.routes').then((m) => m.routes),
+  },
+  /*  
+ 
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./modules/admin/admin.routes').then((m) => m.routes),
+  }, */
+];
