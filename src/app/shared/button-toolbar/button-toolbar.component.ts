79003077686873d908a8877cfbaa149ef779f94b
Anedvago <61481830+Anedvago@users.noreply.github.com>
2023-10-05 09:23:47 -0500

    prev buttons

--- src/app/shared/button-toolbar/button-toolbar.component.ts
+++ src/app/shared/button-toolbar/button-toolbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -9,4 +9,14 @@ import { MatButtonModule } from '@angular/material/button';
   templateUrl: './button-toolbar.component.html',
   styleUrls: ['./button-toolbar.component.css'],
 })
-export class ButtonToolbarComponent {}
+export class ButtonToolbarComponent {
+  @Input()
+  public active: boolean = false;
+
+  @Output()
+  public click: EventEmitter<void> = new EventEmitter<void>();
+
+  public clicked(): void {
+    this.click.emit();
+  }
+}
--- src/app/shared/toolbar/toolbar.component.ts
+++ src/app/shared/toolbar/toolbar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonToolbarComponent } from '../button-toolbar/button-toolbar.component';
 import { ButtonToolbarToggleComponent } from '../button-toolbar-toggle/button-toolbar-toggle.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
@@ -13,27 +14,26 @@ import { ButtonToolbarToggleComponent } from '../button-toolbar-toggle/button-to
 export class ToolbarComponent {
   public buttons = [
     { name: 'inicio', active: true, route: 'dashboard' },
-    {
-      name: 'inventario',
-      active: false,
-      route: 'inventory',
-      children: [
-        { name: 'Articulos', active: false, route: 'inventory/articles' },
-        { name: 'Servicios', active: false, route: 'inventory/services' },
-        { name: 'Categorias', active: false, route: 'inventory/categories' },
-        { name: 'Compras', active: false, route: 'inventory/purchases' },
-        { name: 'Bajas', active: false, route: 'inventory/discharged' },
-      ],
-    },
-    {
-      name: 'reservaciones',
-      active: false,
-      route: 'reservations',
-      children: [
-        { name: 'Habitaciones', active: false, route: 'reservations/rooms' },
-        { name: 'Checks', active: false, route: 'reservations/checks' },
-      ],
-    },
+    { name: 'inventario', active: false, route: 'inventory' },
+    { name: 'reservaciones', active: false, route: 'reservations' },
     { name: 'punto de venta', active: false, route: 'sales' },
   ];
+
+  public buttonsDropDownInventory = [
+    { name: 'Articulos', active: false, route: 'inventory/articles' },
+    { name: 'Servicios', active: false, route: 'inventory/services' },
+    { name: 'Categorias', active: false, route: 'inventory/categories' },
+    { name: 'Compras', active: false, route: 'inventory/purchases' },
+    { name: 'Bajas', active: false, route: 'inventory/discharged' },
+  ];
+  public buttonsDropDownReservations = [
+    { name: 'Habitaciones', active: false, route: 'reservations/rooms' },
+    { name: 'Checks', active: false, route: 'reservations/checks' },
+  ];
+
+  constructor(private router: Router) {}
+
+  public tabClicked(btn: any): void {
+    this.router.navigate([`/admin/${btn.route}`]);
+  }
 }
